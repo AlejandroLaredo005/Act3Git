4f23e943ec4bc18417b805b6ca1b74b20fd88651
Alejandro Laredo <a_alejandro.laredo.de.los.reyes@iespablopicasso.es>
2023-11-28 21:55:14 +0100

    Modifico el random

--- src/numAleatorios/Del1Al10.java
+++ src/numAleatorios/Del1Al10.java
@@ -6,7 +6,7 @@ public class Del1Al10 {
   public static void main(String[] args) {
     int i = 1;
     int numeroAleatorio = 0;
-    for(i = 1; i <=20; i++) {
+    for(i = 1; i <=30; i++) {
       Random rand = new Random();
       numeroAleatorio = rand.nextInt(10) + 1;
       System.out.println("Número aleatorio generado " + i + ": " + numeroAleatorio);
